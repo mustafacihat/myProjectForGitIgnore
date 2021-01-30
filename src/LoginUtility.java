40ac5433da4fd372aa152a84dd11a2d24243171f
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-01-30 22:12:20 +0100

    created LoginUtility.java

---
+++ src/LoginUtility.java
@@ -0,0 +1,7 @@
+public class LoginUtility {
+
+    public static void login(String email, String password){
+        System.out.println("user logged in with this email " + email
+                + " this password" + password);
+    }
+}
